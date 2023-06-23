4b8596fc8e4ccc2c2367a72e2ff807803d1dd4fe
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2023-06-23 10:05:32 +0400

    - Added Dto Validations - Updated the UpdateCoffeeDto - Updated the coffees controller - Updated the coffees service - Added ValidationPipe in the global pipes - Installed @nestjs/mapped-types, class-transformer, and class-validator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,8 +9,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
-import { CreateCoffeeDto } from "./dto/create-coffee.dto";
-import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -23,12 +23,14 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeesService.create(createCoffeeDto);
   }
 
